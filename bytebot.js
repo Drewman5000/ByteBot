d197b6f3e3edb1169851893dab62bc4bb9e903b6
Drewman5000 <andrewrhyland@gmail.com>
2020-10-04 20:30:58 -0700

    update code
    Prettier used on code

--- bytebot.js
+++ bytebot.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const { prefix, author } = require('./config.json');
-const Discord = require('discord.js');
+require("dotenv").config();
+const { prefix, author } = require("./config.json");
+const Discord = require("discord.js");
 const bytebot = new Discord.Client();
 bytebot.commands = new Discord.Collection();
-const bytebotCommands = require('./commands');
+const bytebotCommands = require("./commands");
 
-Object.keys(bytebotCommands).map(key => {
+Object.keys(bytebotCommands).map((key) => {
   bytebot.commands.set(bytebotCommands[key].name, bytebotCommands[key]);
 });
 
@@ -13,30 +13,28 @@ const TOKEN = process.env.TOKEN;
 
 bytebot.login(TOKEN);
 
-bytebot.on('ready', () => {
+bytebot.on("ready", () => {
   console.info(`Logged in as ${bytebot.user.tag}!`);
 });
 
-bytebot.on('message', msg => {
+bytebot.on("message", (msg) => {
   const args = msg.content.split(/\s+/);
-  for ( el of args ) {
-    for ( c of el ) {
+  for (el of args) {
+    for (c of el) {
       if (c.toUpperCase() != c.toLowerCase() || c.codePointAt(0) > 127) {
-          c.toLowerCase;
-        }
+        c.toLowerCase;
+      }
     }
   }
   commandFilter = (arr, query) => {
-    return arr.filter(function(el) {
-      return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
-    })
-  }
-  let commands = (commandFilter(args, prefix));
+    return arr.filter(function (el) {
+      return el.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    });
+  };
+  let commands = commandFilter(args, prefix);
 
-  for ( el of commands ) {
-
+  for (el of commands) {
     if (el.substring(0, 1) == prefix) {
-
       const command = el.substring(1);
       console.info(`Called command: ${command}`);
 
@@ -46,8 +44,8 @@ bytebot.on('message', msg => {
         bytebot.commands.get(command).execute(msg, args);
       } catch (error) {
         console.error(error);
-        msg.reply('There was an error trying to execute that command!');
-      };
+        msg.reply("There was an error trying to execute that command!");
+      }
     }
   }
 });
--- commands/byte.js
+++ commands/byte.js
@@ -1,14 +1,14 @@
 module.exports = {
-	name: 'byte',
-	description: 'Yes or No.',
+	name: "byte",
+	description: "Yes or No.",
 	execute(msg, args) {
 		//msg.reply('pong');
 		const coinFlip = (min, max) => {
-  			return Math.floor(Math.random() * (max - min + 1) ) + min;
-		}
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		};
 		let reply = "";
 		let result = coinFlip(0, 1);
-		reply = (result) ? "Yes. :white_check_mark:" : "No. :no_entry_sign:";
+		reply = result ? "Yes. :white_check_mark:" : "No. :no_entry_sign:";
 
 		msg.channel.send(reply);
 	},
