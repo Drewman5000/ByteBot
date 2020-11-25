c6cbd1e975e482fbe1c21e938683288b57f799a8
Drewman5000 <andrewrhyland@gmail.com>
2020-11-24 17:04:09 -0800

    update version 1.1.5
    Add custom emoji reactions to byte command.

--- commands/byte.js
+++ commands/byte.js
@@ -1,19 +1,26 @@
 module.exports = {
-	name: "byte",
-	description: "Yes or No.",
+	name: 'byte',
+	description: 'Yes or No.',
 	execute(msg, args) {
-		const choices = require("./eightBallOptions.json");
+		const choices = require('./eightBallOptions.json');
 		const coinFlip = (min, max) => {
 			return Math.floor(Math.random() * (max - min + 1)) + min;
 		};
-		let reply = "";
+		let reply = '';
 		let publish = choices.no[coinFlip(0, 9)];
-		let result = coinFlip(0, 1);
+		const result = coinFlip(0, 1);
 		if (result) {
 			publish = choices.yes[coinFlip(0, 5)];
 		}
 
-		reply = result ? "Yes. " + publish : "No. " + publish;
+		if (result) {
+			msg.react('777681011965689876');
+			reply = 'Yes. ' + publish;
+		}
+		else {
+			msg.react('777681038402650143');
+			reply = 'No. ' + publish;
+		}
 
 		msg.channel.send(reply);
 	},
